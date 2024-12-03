5606a325596d45f15a3eb17c0aceb1d7d22bfe1d
Shaun Brown <shaunhbrown@outlook.com>
2024-12-03 19:45:59 +0200

    Finished day 2

--- src/days/day2/tests.ts
+++ src/days/day2/tests.ts
@@ -1,13 +1,16 @@
 import { assertEquals } from "https://deno.land/std@0.208.0/assert/mod.ts";
 import { day2 as solution } from "./index.ts";
 
-const sampleInput = `your
-sample
-input`;
+const sampleInput = `7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9`;
 
 const expected = {
-  part1: "expected result 1",
-  part2: "expected result 2",
+  part1: 2,
+  part2: 4,
 };
 
 Deno.test("Day 2", async (t) => {
@@ -23,17 +26,17 @@ Deno.test("Day 2", async (t) => {
     });
   });
 
-  await t.step("puzzle input", async (t) => {
-    const input = await Deno.readTextFile("src/days/day1/input.txt");
+  // await t.step("puzzle input", async (t) => {
+  //   const input = await Deno.readTextFile("src/days/day1/input.txt");
 
-    await t.step("part 1", async () => {
-      const result = await solution.part1(input);
-      assertEquals(result, "puzzle answer 1"); // Update after solving
-    });
+  //   await t.step("part 1", async () => {
+  //     const result = await solution.part1(input);
+  //     assertEquals(result, "puzzle answer 1"); // Update after solving
+  //   });
 
-    await t.step("part 2", async () => {
-      const result = await solution.part2(input);
-      assertEquals(result, "puzzle answer 2"); // Update after solving
-    });
-  });
+  //   await t.step("part 2", async () => {
+  //     const result = await solution.part2(input);
+  //     assertEquals(result, "puzzle answer 2"); // Update after solving
+  //   });
+  // });
 });
