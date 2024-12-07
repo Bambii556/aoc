ba38ef4e2ea2d1214bdad3e2711abb8e04b86e1f
Shaun Brown <shaunhbrown@outlook.com>
2024-12-07 07:46:39 +0200

    finished day 7

--- src/days/day7/tests.ts
+++ src/days/day7/tests.ts
@@ -1,13 +1,19 @@
 import { assertEquals } from "https://deno.land/std@0.208.0/assert/mod.ts";
 import { day7 as solution } from "./index.ts";
 
-const sampleInput = `your
-sample
-input`;
+const sampleInput = `190: 10 19
+3267: 81 40 27
+83: 17 5
+156: 15 6
+7290: 6 8 6 15
+161011: 16 10 13
+192: 17 8 14
+21037: 9 7 18 13
+292: 11 6 16 20`;
 
 const expected = {
-  part1: "expected result 1",
-  part2: "expected result 2",
+  part1: 3749,
+  part2: 11387,
 };
 
 Deno.test("Day 7", async (t) => {
@@ -20,20 +26,6 @@ Deno.test("Day 7", async (t) => {
     await t.step("part 2", async () => {
       const result = await solution.part2(sampleInput);
       assertEquals(result, expected.part2);
-    });
-  });
-
-  await t.step("puzzle input", async (t) => {
-    const input = await Deno.readTextFile("src/days/day1/input.txt");
-
-    await t.step("part 1", async () => {
-      const result = await solution.part1(input);
-      assertEquals(result, "puzzle answer 1"); // Update after solving
-    });
-
-    await t.step("part 2", async () => {
-      const result = await solution.part2(input);
-      assertEquals(result, "puzzle answer 2"); // Update after solving
     });
   });
 });
