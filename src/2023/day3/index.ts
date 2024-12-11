226b5c71e1bbf1507fb32b5f4960cdcd404939bb
Shaun Brown <shaunhbrown@outlook.com>
2024-12-11 21:06:45 +0200

    Added day 3 template

---
+++ src/2023/day3/index.ts
@@ -0,0 +1,16 @@
+import { getInputLines } from "../../utils/index.ts";
+import { Solution } from "../../types.ts";
+
+export const day3: Solution = {
+  part1: async (input: string) => {
+    const lines = getInputLines(input);
+
+    return 0;
+  },
+
+  part2: async (input: string) => {
+    const lines = getInputLines(input);
+
+    return 0;
+  },
+};
