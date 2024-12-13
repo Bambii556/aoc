8dddf4f0984e6dd889fe936e0ac0e9ca5524d370
Shaun Brown <shaunhbrown@outlook.com>
2024-12-13 16:28:51 +0200

    Scaffolded day 6 2023

---
+++ src/2023/day6/test.ts
@@ -0,0 +1,36 @@
+import { DayTests, runTestPart } from "../../utils/test_utils.ts";
+import { day6 as solution } from "./index.ts";
+import { log } from "../../utils/index.ts";
+
+const input = ``;
+
+const tests: DayTests = {
+  part1: [
+    {
+      input,
+      expected: 0,
+    },
+  ],
+  part2: [
+    // {
+    //   input,
+    //   expected: 0,
+    // },
+  ],
+};
+
+export async function runPart1Tests() {
+  if (!tests.part1?.length) {
+    log("⚠️ No tests defined for Part 1\n");
+    return true;
+  }
+  return await runTestPart(solution.part1, tests.part1, 1);
+}
+
+export async function runPart2Tests() {
+  if (!tests.part2?.length) {
+    log("⚠️ No tests defined for Part 2\n");
+    return true;
+  }
+  return await runTestPart(solution.part2, tests.part2, 2);
+}
