bf992824de039750594ac749ff8d01215a3f6a69
Shaun Brown <shaunhbrown@outlook.com>
2024-12-18 14:28:20 +0200

    wip day8 2023

--- src/2023/day8/test.ts
+++ src/2023/day8/test.ts
@@ -2,13 +2,21 @@ import { DayTests, runTestPart } from "../../utils/test_utils.ts";
 import { day8 as solution } from "./index.ts";
 import { log } from "../../utils/index.ts";
 
-const input = ``;
+const input = `RL
+
+AAA = (BBB, CCC)
+BBB = (DDD, EEE)
+CCC = (ZZZ, GGG)
+DDD = (DDD, DDD)
+EEE = (EEE, EEE)
+GGG = (GGG, GGG)
+ZZZ = (ZZZ, ZZZ)`;
 
 const tests: DayTests = {
   part1: [
     {
       input,
-      expected: 0,
+      expected: 6,
     },
   ],
   part2: [
