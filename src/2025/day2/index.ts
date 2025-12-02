11f0d4bb6f5c6a50ed76faf65e1ad84ea17a11bf
Shaun Brown <ShaunBrown@capitecbank.co.za>
2025-12-02 09:35:01 +0200

    Finished day 2 2025

--- src/2025/day2/index.ts
+++ src/2025/day2/index.ts
@@ -3,12 +3,63 @@ import { Solution } from "../../types.ts";
 
 export const day2: Solution = {
   part1: async (input: string) => {
-    const lines = getInputLines(input);
-    return "";
+    const ids = getInputLines(input)[0].split(",").map((id) => ({
+      start: parseInt(id.split("-")[0]),
+      end: parseInt(id.split("-")[1]),
+    }));
+    let sum = 0;
+
+    ids.forEach((id) => {
+      for (let i = id.start; i <= id.end; i++) {
+        const num = i;
+        const item = num.toString();
+        if (item.length % 2 === 0) {
+          const left = item.slice(0, item.length / 2);
+          const right = item.slice(item.length / 2);
+          if (left === right) {
+            sum += num;
+          }
+        }
+      }
+    });
+
+    return sum;
   },
 
   part2: async (input: string) => {
-    const lines = getInputLines(input);
-    return "";
+    const ids = getInputLines(input)[0].split(",").map((id) => ({
+      start: parseInt(id.split("-")[0]),
+      end: parseInt(id.split("-")[1]),
+    }));
+    let sum = 0;
+
+    ids.forEach((id) => {
+      for (let i = id.start; i <= id.end; i++) {
+        const num = i;
+        if (hasRepeatingPattern(num)) {
+          sum += num;
+        }
+      }
+    });
+
+    return sum;
   },
 };
+
+function hasRepeatingPattern(num: number): boolean {
+  const str = num.toString();
+  const len = str.length;
+
+  for (let patternLen = 1; patternLen <= len / 2; patternLen++) {
+    if (len % patternLen === 0) {
+      const pattern = str.substring(0, patternLen);
+      const repetitions = len / patternLen;
+
+      if (pattern.repeat(repetitions) === str) {
+        return true;
+      }
+    }
+  }
+
+  return false;
+}
--- src/2025/day2/test.ts
+++ src/2025/day2/test.ts
@@ -2,19 +2,20 @@ import { DayTests, runTestPart } from "../../utils/test_utils.ts";
 import { day2 as solution } from "./index.ts";
 import { log } from "../../utils/index.ts";
 
-const input = ``;
+const input =
+  `11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124`;
 
 const tests: DayTests = {
   part1: [
     {
       input,
-      expected: 0,
+      expected: 1227775554,
     },
   ],
   part2: [
     {
       input,
-      expected: 0,
+      expected: 4174379265,
     },
   ],
 };
