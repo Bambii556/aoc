259fb8a437e5cc72969fb44368a560a0b637d822
Shaun Brown <ShaunBrown@capitecbank.co.za>
2025-12-03 17:42:43 +0200

    Finished day 9 2023

--- src/2023/day9/index.ts
+++ src/2023/day9/index.ts
@@ -3,14 +3,87 @@ import { Solution } from "../../types.ts";
 
 export const day9: Solution = {
   part1: async (input: string) => {
-    const lines = getInputLines(input);
+    const lines = getInputLines(input).map((line) =>
+      line.split(" ").map(Number)
+    );
+    let sum = 0;
+    for (const line of lines) {
+      const lastDigits: number[] = [];
+      lastDigits.push(line[line.length - 1]);
+      let currentSum = 0;
+      console.log(`Starting line: ${JSON.stringify(line)}`);
+      let diffs: number[] = [];
+      do {
+        currentSum = 0;
 
-    return 0;
+        diffs = extrapolate(diffs.length === 0 ? line : diffs);
+        console.log(JSON.stringify(diffs));
+        lastDigits.push(diffs[diffs.length - 1]);
+
+        currentSum = diffs.reduce((a, b) => a + b, 0);
+      } while (currentSum !== 0);
+
+      const finalValue = lastDigits.reduce((a, b) => a + b, 0);
+      console.log(
+        `Final value for line: ${JSON.stringify(lastDigits)} => ${finalValue}`,
+      );
+      console.log("-------------------");
+      sum += finalValue;
+    }
+
+    console.log(`Final: ${sum}`);
+    return sum;
   },
 
   part2: async (input: string) => {
-    const lines = getInputLines(input);
+    const lines = getInputLines(input).map((line) =>
+      line.split(" ").map(Number)
+    );
+    let sum = 0;
+    for (const line of lines) {
+      const firstDigits: number[] = [];
+      firstDigits.push(line[0]);
+      let currentSum = 0;
+      console.log(`Starting line: ${JSON.stringify(line)}`);
+      let diffs: number[] = [];
+      do {
+        currentSum = 0;
 
-    return 0;
+        diffs = extrapolate(diffs.length === 0 ? line : diffs);
+        console.log(JSON.stringify(diffs));
+        firstDigits.push(diffs[0]);
+
+        currentSum = diffs.reduce((a, b) => a + b, 0);
+      } while (currentSum !== 0);
+
+      // Reverse
+      firstDigits.reverse();
+
+      let nextVal = 0;
+      for (let i = 0; i < firstDigits.length; i++) {
+        const first = firstDigits[i];
+        nextVal = first - nextVal;
+      }
+      sum += nextVal;
+    }
+
+    return sum;
   },
 };
+
+function extrapolate(nums: number[]): number[] {
+  const result: number[] = [];
+  const len = nums.length;
+
+  for (let i = 0; i < len; i++) {
+    if (i + 1 >= len) break;
+
+    const current = nums[i];
+    const next = nums[i + 1];
+
+    const diff = next - current;
+    result.push(diff);
+  }
+
+  return result;
+}
--- src/2023/day9/test.ts
+++ src/2023/day9/test.ts
@@ -2,19 +2,46 @@ import { DayTests, runTestPart } from "../../utils/test_utils.ts";
 import { day9 as solution } from "./index.ts";
 import { log } from "../../utils/index.ts";
 
-const input = ``;
+const input1 = `0 3 6 9 12 15`;
+const input2 = `1 3 6 10 15 21`;
+const input3 = `10 13 16 21 30 45`;
+const input4 = `${input1}\n${input2}\n${input3}`;
 
 const tests: DayTests = {
   part1: [
     {
-      input,
-      expected: 0,
+      input: input1,
+      expected: 18,
+    },
+    {
+      input: input2,
+      expected: 28,
+    },
+    {
+      input: input3,
+      expected: 68,
+    },
+    {
+      input: input4,
+      expected: 114,
     },
   ],
   part2: [
     {
-      input,
+      input: input3,
+      expected: 5,
+    },
+    {
+      input: input1,
+      expected: -3,
+    },
+    {
+      input: input2,
       expected: 0,
+    },
+    {
+      input: input4,
+      expected: 2,
     },
   ],
 };
